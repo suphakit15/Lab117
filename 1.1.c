e34a6ebeef68834db4d3ba202181efaac0c515f7
suphakit15 <mokzung26@gmail.com>
2025-12-03 13:11:11 +0700

    Add files via upload

--- 1.1.c
+++ 1.1.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    int N ,i;
-    if (scanf("%d", &N) != 1) {  /* รับค่า จำนวน N */
-        return 1;
-    }
-    for (i = 1; i <= N; i++) { /*แสดงข้อความตามจำนวน N */
-        printf("[%d] Hello world\n", i);
+    int N = 0; 
+    int i = 0;
+    
+    scanf("%d", &N);/* รับค่า จำนวน N */
+
+
+    for (i = 0; i < N; i++) { /*แสดงข้อความตามจำนวน N */
+        printf("[%d] Hello world\n", i + 1 );
     }
 
     return 0;
