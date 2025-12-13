7ad227988b76d77d6c0490abfc4c39c52db18bd7
suphakit15 <mokzung26@gmail.com>
2025-12-13 15:54:25 +0700

    Add files via upload

--- 2.4.c
+++ 2.4.c
@@ -1,43 +1,36 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 
-void explode(char str1[], char splitter, char str2[][10], int *count) {
-    int len = strlen(str1);
-    int i, j, k;
-    *count = 0;
-    i = 0;
-    k = 0;
+void explode(char str1[], char splitter, char str2[][10], int *count);
 
-    for (j = 0; j <= len; j++) {
-        if (str1[j] == splitter || str1[j] == '\0') {
-            str2[i][k] = '\0';
-            i++;
-            k = 0;
-            
-            if (i >= 20) {
-                break;
-            }
-        } else {
-            if (k < 9) {
-                str2[i][k] = str1[j];
-                k++;
-            }
-        }
+int main() {
+    char out[20][10];
+    int num;
+    
+    explode("I/Love/You", '/', out, &num);
+    for (int i = 0; i < num; i++) {
+        printf("str2[%d] = \"%s\"\n", i, out[i]);
     }
     
-    *count = i;
+    printf("count = %d\n", num);
+    return 0;
 }
 
-int main() { 
-    char out[20][10];
-    int num; 
-    explode("I/love/you", '/', out, &num);
-    
-    printf("จะได้ผลลัพธ์คือ:\n");
-    for (int i = 0; i < num; i++) {
-        printf("str2[%d] = %s\n", i, out[i]);
+void explode(char str1[], char splitter, char str2[][10], int *count) {
+    int i = 0;
+    int j = 0;
+    int k = 0;
+    int len = strlen(str1);
+    *count = 0;
+    for (i = 0; i <= len; i++) {
+        if (str1[i] == splitter || str1[i] == '\0') {
+            str2[k][j] = '\0';
+            k++;
+            (*count)++;
+            j = 0;
+        } else {
+            str2[k][j] = str1[i];
+            j++;
+        }
     }
-    printf("count = %d\n", num);
-    
-    return 0;
 }
